eeac147255099c96b5ffa7d88fb8949f3f29353e
Zinou-web <zinou.degh111@gmail.com>
2025-12-31 23:23:17 +0100

    Update nfa_converter.c

--- nfa_converter.c
+++ nfa_converter.c
@@ -1,74 +1,45 @@
+//DEGHNOUCHE ZIN EL ABIDINE "G01 AI"
+
 #include <stdio.h>
-
-
 
 #define MAX 10
 
-
-
 int n, m;
-
 char alpha[MAX];
-
 int start;
 
-
-
 int final[MAX];
-
 int newFinal[MAX];
 
-
-
 int eps[MAX][MAX];
-
 int trans[MAX][MAX][MAX];
 
-
-
 int eclose[MAX][MAX];
-
 int newTrans[MAX][MAX][MAX];
-
-
 
 // Task 1: Read NFA
 
 void readNFA() {
 
     int i, j, k;
-
-
-
     scanf("%d", &n);
-
     scanf("%d", &m);
-
-
 
     for (i = 0; i < m; i++)
 
         scanf(" %c", &alpha[i]);
 
-
-
     scanf("%d", &start);
-
-
 
     for (i = 0; i < n; i++)
 
         scanf("%d", &final[i]);
-
-
 
     for (i = 0; i < n; i++)
 
         for (j = 0; j < n; j++)
 
             scanf("%d", &eps[i][j]);
-
-
 
     for (i = 0; i < n; i++)
 
@@ -80,25 +51,18 @@ void readNFA() {
 
 }
 
-
-
 // Task 2: Epsilon Closure
 
-// Helper function to perform DFS for epsilon closure
 void dfsEpsilon(int state, int visited[]) {
 
     int i;
 
-    
-
     visited[state] = 1;
-
-    
 
     for (i = 0; i < n; i++)
 
         if (eps[state][i] && !visited[i])
-
+            
             dfsEpsilon(i, visited);
 
 }
@@ -109,27 +73,13 @@ void epsilonClosure() {
 
     int visited[MAX];
 
-
-
-    // For each state, compute its epsilon closure using DFS
-
     for (i = 0; i < n; i++) {
-
-        // Initialize visited array for this DFS
 
         for (j = 0; j < n; j++)
 
             visited[j] = 0;
 
-        
-
-        // Perform DFS starting from state i
-
         dfsEpsilon(i, visited);
-
-        
-
-        // Copy visited states to epsilon closure
 
         for (j = 0; j < n; j++)
 
@@ -139,15 +89,11 @@ void epsilonClosure() {
 
 }
 
-
-
 // Task 3: New Transitions
 
 void buildNewTransitions() {
 
     int i, j, x, y, z, k;
-
-
 
     for (i = 0; i < n; i++)
 
@@ -158,11 +104,7 @@ void buildNewTransitions() {
                 newTrans[i][j][k] = 0;
 
 
-
-    // For state i on symbol j: 
-    // - Start from all states in epsilon closure of i
-    // - Apply transition on symbol j
-    // - Include all states in epsilon closure of the result
+//.
     for (i = 0; i < n; i++)
 
         for (j = 0; j < m; j++)
@@ -192,7 +134,6 @@ void buildNewFinalStates() {
     int i, j;
 
 
-
     for (i = 0; i < n; i++) {
 
         newFinal[i] = 0;
@@ -211,21 +152,15 @@ void buildNewFinalStates() {
 
 }
 
-
-
 // Task 5: Display
 
 void display() {
 
     int i, j, k;
 
-
-
     for (i = 0; i < n; i++)
 
         printf("Final(%d) = %d\n", i, newFinal[i]);
-
-
 
     for (i = 0; i < n; i++)
 
@@ -244,8 +179,6 @@ void display() {
         }
 
 }
-
-
 
 // Main
 
